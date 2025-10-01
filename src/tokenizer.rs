987d4da144b2bbbe8e4d8de2a91ae6dc44d32c58
I had a bad idea <182666156+I-had-a-bad-idea@users.noreply.github.com>
2025-10-01 15:11:30 +0200

    Value based tokenizer

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,5 +1,5 @@
 use regex::Regex;
-use std::collections::HashMap;
+use std::{collections::HashMap};
 
 use crate::{debug::EvaluatioError, evaluator::Value, built_in_functions::BUILT_IN_FUNCTIONS};
 
@@ -11,7 +11,7 @@ impl Tokenizer{
         Self {
         }
     }
-    pub fn tokenize(&self, input: &str, variables: HashMap<String, Value>, functions: HashMap<String, HashMap<String, Value>>) -> Vec<String> {
+    pub fn tokenize(&self, input: &str, variables: HashMap<String, Value>, functions: HashMap<String, HashMap<String, Value>>) -> Vec<Value> {
         // Placeholder for tokenization logic
         let tokens = self.split(input);
         //println!("tokens after splitting: {:?}", tokens);
@@ -28,52 +28,84 @@ impl Tokenizer{
             .collect();
         return tokens;
     }
-        fn shunting_yard(&self, tokens: Vec<String>, variables: HashMap<String, Value>, functions: HashMap<String, HashMap<String, Value>>) -> Vec<String> {
+        fn shunting_yard(&self, tokens: Vec<String>, variables: HashMap<String, Value>, functions: HashMap<String, HashMap<String, Value>>) -> Vec<Value> {
         let prec = HashMap::from([
             ("or", 1), ("and", 2),
             ("==", 3), ("!=", 3), ("<", 3), ("<=", 3), (">", 3), (">=", 3),
             ("+", 4), ("-", 4),
             ("*", 5), ("/", 5)
         ]);
-        let mut output: Vec<String> = vec![];
-        let mut stack: Vec<String> = vec![];
+        let mut output: Vec<Value> = vec![];
+        let mut stack: Vec<Value> = vec![];
 
-        for token in &tokens {
+        let mut i = 0;
+        while i < tokens.len(){
+            let token = &tokens[i];
             if token.starts_with('"') && token.ends_with('"') || token.starts_with("'") && token.ends_with("'"){
-                output.push(token.clone());
+                output.push(Value::Str(token.clone()));
             }
-            else if token.trim_matches('.').parse::<f64>().is_ok() || variables.contains_key(token) || functions.contains_key(token) || BUILT_IN_FUNCTIONS.contains_key(&token as &str){
-                output.push(token.clone());
+            else if token.trim_matches('.').parse::<f64>().is_ok() || variables.contains_key(token){
+                output.push(Value::Str(token.clone()));
+            }
+            else if functions.contains_key(token) || BUILT_IN_FUNCTIONS.contains_key(&token as &str){
+                if tokens.get(i+1) != Some(&"(".to_string()){
+                    EvaluatioError::new(format!("Function {} must be followed by (", token), None, None).raise();
+                }
+                output.push(Value::Str(token.clone()));
+                i += 1; // Skip the next token which is '('
+                let mut function_arguments = vec![];
+                let mut argument: String = String::new();
+                while let Some(next_token) = tokens.get(i+1) {
+                    if next_token == ")" {
+                        i += 1;
+                        break;
+                    }
+                    else if next_token == "," {
+                        if argument.is_empty() {
+                            i += 1;
+                            continue;
+                        }
+                        println!("Pushing argument: {}", argument);
+                        function_arguments.push(Value::Str(argument.clone()));
+                        i += 2;
+                    }
+                    else {
+                        argument += next_token;
+                        i += 1;
+                    }
+                }
+                function_arguments.push(Value::Str(argument.clone()));
+                output.push(Value::List(function_arguments));
             }
             else if prec.contains_key(token.as_str()) {
                 while let Some(last) = stack.last() {
-                    if prec.contains_key(last.as_str()) && prec[last.as_str()] >= prec[token.as_str()]{
+                    if prec.contains_key(last.to_string_value().as_str()) && prec[last.to_string_value().as_str()] >= prec[token.as_str()]{
                         output.push(stack.pop().unwrap());
                     }
                     else{
                         break;
                     }
                 }
-                stack.push(token.clone());
+                stack.push(Value::Str(token.clone()));
             }
             else if token == "("{
-                stack.push(token.clone());
+                stack.push(Value::Str(token.clone()));
             }
             else if token == "," {
                 // Pop operators until we reach a left parenthesis
                 while let Some(last) = stack.last() {
-                    if last != "(" {
+                    if last.to_string_value() != "(" {
                         output.push(stack.pop().unwrap());
                     } else {
                         break;
                     }
                 }
                 // Add the comma to the output
-                output.push(token.clone());
+                output.push(Value::Str(token.clone()));
             }
             else if token == ")"{
                 while let Some(last) = stack.last(){
-                    if last != "("{
+                    if last.to_string_value() != "("{
                         output.push(stack.pop().unwrap());
                     }
                     else{
@@ -88,10 +120,13 @@ impl Tokenizer{
             else{
                 EvaluatioError::new(format!("Unknown token {}", token), None, None).raise();
             }
+            i += 1;
         }
 
+        println!("Stack after processing tokens: {:?}", stack);
+        println!("Output after processing tokens: {:?}", output);
         while let Some(last) = stack.last(){
-            if last == "(" || last == ")"{
+            if last.to_string_value() == "(" || last.to_string_value() == ")"{
                 EvaluatioError::new("Mismatched parentheses".to_string(), None, None).raise();
             }
             output.push(stack.pop().unwrap());
